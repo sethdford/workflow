9bbb6ad3c92bcbf0e5e8b5cd814368dbc6bd11a4
Seth Ford <seth.ford@fmr.com>
2017-02-04 13:04:47 -0700

    updated

--- examples/config.js
+++ examples/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    awsRegion: 'us-east-1',
-    awsAccessKey: 'AKIAJDWN7XVA5AIX3JMQ',
-    awsSecretKey: 'Q/tKNRcVAOYjHFoOEf43WeLH77dAyStIb5uvyyQS'
+    awsRegion: '',
+    awsAccessKey: '',
+    awsSecretKey: ''
 }
